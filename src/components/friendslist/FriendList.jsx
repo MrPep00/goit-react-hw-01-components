c2a2821efab41347fe9d04ce4167f8968c83b709
MrPep00 <gleb.plotnikov00@gmail.com>
2023-07-11 21:08:47 +0200

    summary

---
+++ src/components/friendslist/FriendList.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  const statusOnline = isOnline ? 'online' : 'offline';
+  return (
+    <li className={css.item}>
+      <span className={css[statusOnline]}></span>
+      <img class={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+};
+
+export const Friendslist = ({ friends }) => (
+  <ul className={css.friend_list}>
+    {friends.map(friend => (
+      <FriendListItem
+        key={friend.id}
+        avatar={friend.avatar}
+        name={friend.name}
+        isOnline={friend.isOnline}
+      />
+    ))}
+  </ul>
+);
+
+Friendslist.propTypes = {
+  friends: PropTypes.array,
+};
---
+++ src/components/transaction/TransactionHistory.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import css from './Transaction.module.css';
+
+export const TransactionHistory = ({ items }) => (
+  <table className={css.transaction_history}>
+    <thead>
+      <tr>
+        <th>Type</th>
+        <th>Amount</th>
+        <th>Currency</th>
+      </tr>
+    </thead>
+    <tbody>
+      {items.map(item => (
+        <tr key={item.id}>
+          <td>{item.type}</td>
+          <td>{item.amount}</td>
+          <td>{item.currency}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
+
+TransactionHistory.propTypes = {
+  items: PropTypes.array,
+};
